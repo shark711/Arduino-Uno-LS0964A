4fdfd73ec3071c0e562e202eb9d2422987e95137
shark711 <wallace.roets@gmail.com>
2021-01-01 11:50:02 +1100

    Update LS0964A.ino

--- LS0964A/LS0964A.ino
+++ LS0964A/LS0964A.ino
@@ -11,66 +11,98 @@ void setup()
 
 void loop()
 {
+  //RGB #ff0000 - RED
   analogWrite(9, 255);
   analogWrite(10, 0);
   analogWrite(11, 0);
   delay(1000); // Wait for 1000 millisecond(s)
+  
+  //RGB #000000 - BLACK
   analogWrite(9, 0);
   analogWrite(10, 0);
   analogWrite(11, 0);
   delay(1000); // Wait for 1000 millisecond(s)
+  
+  //RGB #00ff00 - GREEN
   analogWrite(9, 0);
   analogWrite(10, 255);
   analogWrite(11, 0);
   delay(1000); // Wait for 1000 millisecond(s)
+  
+  //RGB #000000 - BLACK
   analogWrite(9, 0);
   analogWrite(10, 0);
   analogWrite(11, 0);
   delay(1000); // Wait for 1000 millisecond(s)
+ 
+  //RGB #0000ff - BLUE
   analogWrite(9, 0);
   analogWrite(10, 0);
   analogWrite(11, 255);
   delay(1000); // Wait for 1000 millisecond(s)
+  
+  //RGB #000000 - BLACK
   analogWrite(9, 0);
   analogWrite(10, 0);
   analogWrite(11, 0);
   delay(1000); // Wait for 1000 millisecond(s)
+  
+  //RGB #ffffff - WHITE
   analogWrite(9, 255);
   analogWrite(10, 255);
   analogWrite(11, 255);
   delay(1000); // Wait for 1000 millisecond(s)
+  
+  //RGB #000000 - BLACK
   analogWrite(9, 0);
   analogWrite(10, 0);
   analogWrite(11, 0);
   delay(1000); // Wait for 1000 millisecond(s)
+  
+  //Fade from R -> G -> -> R 10 times, starting with a 400ms delay, and going 40ms faster every loop
   wait_time = 400;
   for (counter = 0; counter < 10; ++counter) {
+    
+    //RGB #ff0000 - RED
     analogWrite(9, 255);
     analogWrite(10, 0);
     analogWrite(11, 0);
     delay(wait_time); // Wait for wait_time millisecond(s)
+    
+    //RGB #ffff00 - YELLOW
     analogWrite(9, 255);
     analogWrite(10, 255);
     analogWrite(11, 0);
     delay(wait_time); // Wait for wait_time millisecond(s)
+    
+    //RGB #00ff00 - GREEN
     analogWrite(9, 0);
     analogWrite(10, 255);
     analogWrite(11, 0);
     delay(wait_time); // Wait for wait_time millisecond(s)
+    
+    //RGB #00ffff - CYAN
     analogWrite(9, 0);
     analogWrite(10, 255);
     analogWrite(11, 255);
     delay(wait_time); // Wait for wait_time millisecond(s)
+    
+    //RGB #0000ff - BLUE
     analogWrite(9, 0);
     analogWrite(10, 0);
     analogWrite(11, 255);
     delay(wait_time); // Wait for wait_time millisecond(s)
+    
+    //RGB #ff00ff - MAGENTA
     analogWrite(9, 255);
     analogWrite(10, 0);
     analogWrite(11, 255);
     delay(wait_time); // Wait for wait_time millisecond(s)
-    wait_time += -40;
-  }
+    //Decrease wait time
+    wait_time -= 40;
+  } //End for (counter)
+  
+  //RGB #000000 - BLACK
   analogWrite(9, 0);
   analogWrite(10, 0);
   analogWrite(11, 0);
